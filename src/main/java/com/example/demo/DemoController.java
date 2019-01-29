dbdcbc10abfa2ed53761a0e0f59868e96f6a7cc0
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-01-29 18:29:32 +0900

    add demo

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,15 +1,28 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class DemoController {
+    @Autowired
+    private DemoRepository demoRepository;
+
     @GetMapping
     public String index(Model model) {
+        model.addAttribute("list", demoRepository.findAll());
         return "index";
+    }
+
+    @PostMapping
+    public String post(@ModelAttribute Demo demo) {
+        demoRepository.save(demo);
+        return "redirect:/";
     }
 }
