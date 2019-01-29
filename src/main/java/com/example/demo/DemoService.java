6394805eb46f075f6395b13a709037014a048847
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-30 08:44:34 +0900

    apply formatter

--- src/main/java/com/example/demo/DemoService.java
+++ src/main/java/com/example/demo/DemoService.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class DemoService {
